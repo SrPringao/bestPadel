1dc34174155b6879eda579e61189b91a9a69e328
SrPringao <franco.aldrete@gmail.com>
2025-03-29 00:34:25 -0600

    añadir clubes

--- backend/clubs.js
+++ backend/clubs.js
@@ -19,4 +19,50 @@ module.exports = [
         tenant_id: "9ac31a4a-d51c-4e91-96d3-6b65fd8ab9a5",
         link: "https://playtomic.io/tenant/9ac31a4a-d51c-4e91-96d3-6b65fd8ab9a5?utm_source=app_ios&utm_campaign=share"
     },
+    {
+        name: "Padel Guadalupe",
+        tenant_id: "fd6f57ae-caa3-4025-8aa1-bb3cfcf476eb",
+        link: "https://playtomic.io/tenant/fd6f57ae-caa3-4025-8aa1-bb3cfcf476eb?utm_source=app_ios&utm_campaign=share"
+    },
+    {
+        name: "Faro Padel Club",
+        tenant_id: "8b38f679-b397-44c9-8b84-058f4663937d",
+        link: "https://playtomic.io/tenant/8b38f679-b397-44c9-8b84-058f4663937d?utm_source=app_ios&utm_campaign=share"
+    },
+    {
+        name: "Padel isla raza",
+        tenant_id: "cf511f35-bc59-44a7-9e37-5ff54ca4761d",
+        link: "https://playtomic.io/tenant/cf511f35-bc59-44a7-9e37-5ff54ca4761d?utm_source=app_ios&utm_campaign=share"
+    },
+    {
+        name: "Complejo Padel AGA",
+        tenant_id: "a8246d61-938f-4e51-ada9-b5f9ec6aabc9",
+        link: "https://playtomic.io/tenant/a8246d61-938f-4e51-ada9-b5f9ec6aabc9?utm_source=app_ios&utm_campaign=share"
+    },
+    {
+        name: "Padel factory",
+        tenant_id: "2e66f694-4d7e-42ef-967f-d8e94b90db1d",
+        link: "https://playtomic.io/tenant/2e66f694-4d7e-42ef-967f-d8e94b90db1d?utm_source=app_ios&utm_campaign=share"
+    },
+    {
+        name: "Providencia Padel Club",
+        tenant_id: "517a8e10-1646-430d-bdcc-5cf34ee32f4b",
+        link: "https://playtomic.io/tenant/517a8e10-1646-430d-bdcc-5cf34ee32f4b?utm_source=app_ios&utm_campaign=share"
+    },
+    {
+        name: "Draft Padel Americas",
+        tenant_id: "64d2fd83-e7ee-4407-a08e-adecf4e49332",
+        link: "https://playtomic.io/tenant/64d2fd83-e7ee-4407-a08e-adecf4e49332?utm_source=app_ios&utm_campaign=share"
+    },
+    {
+        name: "Padel Family Gdl",
+        tenant_id: "010d3779-67e5-4d19-8c59-2c7f029e56a7",
+        link: "https://playtomic.io/tenant/010d3779-67e5-4d19-8c59-2c7f029e56a7?utm_source=app_ios&utm_campaign=share"
+    },
+    {
+        name: "Slice Padel Club",
+        tenant_id: "2d8dff4c-1310-454e-bab5-bd6ee1606655",
+        link: "https://playtomic.io/tenant/2d8dff4c-1310-454e-bab5-bd6ee1606655?utm_source=app_ios&utm_campaign=share"
+    },
+
 ];
