2450c2dc12adcb2adaa2e51d99b28de53a9612fd
BetoVeraRz <alberto.adrian.vera.ruiz@gmail.com>
2025-06-19 14:04:06 -0600

    add new clubs

--- backend/clubs.js
+++ backend/clubs.js
@@ -1,90 +1,140 @@
 module.exports = [
-    {
-        name: "Smash Padel",
-        tenant_id: "1adcdabc-ef3b-438a-adac-47ba21ba5d3a",
-        link: "https://playtomic.io/tenant/1adcdabc-ef3b-438a-adac-47ba21ba5d3a?utm_source=app_ios&utm_campaign=share",
-        zone: "Zapopan",
-        lat: 20.684411479748288,
-        lon: -103.45255844206463
-    },
-    {
-        name: "Padel & Beer",
-        tenant_id: "535818f3-b9ff-45ba-8cae-01e86b225fe6",
-        link: "https://playtomic.io/tenant/535818f3-b9ff-45ba-8cae-01e86b225fe6?utm_source=app_ios&utm_campaign=share",
-        zone: "Guadalajara",
-        lat: 20.66315873404667,
-        lon: -103.4194322513063
-    },
-    {
-        name: "Metropadel",
-        tenant_id: "c0ae672b-3e7b-40fa-8cad-c74e35df8a5f",
-        link: "https://playtomic.io/tenant/c0ae672b-3e7b-40fa-8cad-c74e35df8a5f?utm_source=app_ios&utm_campaign=share",
-        zone: "Guadalajara",
-        lat: 20.678301520846066,
-        lon: -103.44016678033744
-    },
-    {
-        name: "Zander Padel",
-        tenant_id: "9ac31a4a-d51c-4e91-96d3-6b65fd8ab9a5",
-        link: "https://playtomic.io/tenant/9ac31a4a-d51c-4e91-96d3-6b65fd8ab9a5?utm_source=app_ios&utm_campaign=share",
-        zone: "Tlaquepaque",
-        lat: 20.673898243956923, lon: -103.40367219909187
-    },
-    {
-        name: "Padel Guadalupe",
-        tenant_id: "fd6f57ae-caa3-4025-8aa1-bb3cfcf476eb",
-        link: "https://playtomic.io/tenant/fd6f57ae-caa3-4025-8aa1-bb3cfcf476eb?utm_source=app_ios&utm_campaign=share",
-        zone: "Guadalajara",
-        lat: 20.65909078708705, lon: -103.43266338172637
-    },
-    {
-        name: "Faro Padel Club",
-        tenant_id: "8b38f679-b397-44c9-8b84-058f4663937d",
-        link: "https://playtomic.io/tenant/8b38f679-b397-44c9-8b84-058f4663937d?utm_source=app_ios&utm_campaign=share",
-        zone: "Zapopan",
-        lat: 20.64354097626638, lon: -103.39044464232299
-    },
-    {
-        name: "Padel isla raza",
-        tenant_id: "cf511f35-bc59-44a7-9e37-5ff54ca4761d",
-        link: "https://playtomic.io/tenant/cf511f35-bc59-44a7-9e37-5ff54ca4761d?utm_source=app_ios&utm_campaign=share",
-        zone: "Guadalajara",
-        lat: 20.633831337895018, lon: -103.38848901843501
-    },
-    {
-        name: "Padel factory",
-        tenant_id: "2e66f694-4d7e-42ef-967f-d8e94b90db1d",
-        link: "https://playtomic.io/tenant/2e66f694-4d7e-42ef-967f-d8e94b90db1d?utm_source=app_ios&utm_campaign=share",
-        zone: "Zapopan",
-        lat: 20.693354837519717, lon: -103.45211120963569
-    },
-    {
-        name: "Providencia Padel Club",
-        tenant_id: "517a8e10-1646-430d-bdcc-5cf34ee32f4b",
-        link: "https://playtomic.io/tenant/517a8e10-1646-430d-bdcc-5cf34ee32f4b?utm_source=app_ios&utm_campaign=share",
-        zone: "Guadalajara",
-        lat: 20.68659486484572, lon: -103.39369878446952
-    },
-    {
-        name: "Draft Padel Americas",
-        tenant_id: "64d2fd83-e7ee-4407-a08e-adecf4e49332",
-        link: "https://playtomic.io/tenant/64d2fd83-e7ee-4407-a08e-adecf4e49332?utm_source=app_ios&utm_campaign=share",
-        zone: "Zapopan",
-        lat: 20.68759803643518, lon: -103.37462480483806
-    },
-    {
-        name: "Padel Family Gdl",
-        tenant_id: "010d3779-67e5-4d19-8c59-2c7f029e56a7",
-        link: "https://playtomic.io/tenant/010d3779-67e5-4d19-8c59-2c7f029e56a7?utm_source=app_ios&utm_campaign=share",
-        zone: "Tlaquepaque",
-        lat: 20.68827012888777, lon: -103.42775028797583
-    },
-    {
-        name: "Slice Padel Club",
-        tenant_id: "2d8dff4c-1310-454e-bab5-bd6ee1606655",
-        link: "https://playtomic.io/tenant/2d8dff4c-1310-454e-bab5-bd6ee1606655?utm_source=app_ios&utm_campaign=share",
-        zone: "Guadalajara",
-        lat: 20.68463509573624, lon: -103.44440355810076
-    },
+  {
+    name: "Smash Padel",
+    tenant_id: "1adcdabc-ef3b-438a-adac-47ba21ba5d3a",
+    link: "https://playtomic.io/tenant/1adcdabc-ef3b-438a-adac-47ba21ba5d3a?utm_source=app_ios&utm_campaign=share",
+    zone: "Zapopan",
+    lat: 20.684411479748288,
+    lon: -103.45255844206463,
+  },
+  {
+    name: "Padel & Beer",
+    tenant_id: "535818f3-b9ff-45ba-8cae-01e86b225fe6",
+    link: "https://playtomic.io/tenant/535818f3-b9ff-45ba-8cae-01e86b225fe6?utm_source=app_ios&utm_campaign=share",
+    zone: "Guadalajara",
+    lat: 20.66315873404667,
+    lon: -103.4194322513063,
+  },
+  {
+    name: "Metropadel",
+    tenant_id: "c0ae672b-3e7b-40fa-8cad-c74e35df8a5f",
+    link: "https://playtomic.io/tenant/c0ae672b-3e7b-40fa-8cad-c74e35df8a5f?utm_source=app_ios&utm_campaign=share",
+    zone: "Guadalajara",
+    lat: 20.678301520846066,
+    lon: -103.44016678033744,
+  },
+  {
+    name: "Zander Padel",
+    tenant_id: "9ac31a4a-d51c-4e91-96d3-6b65fd8ab9a5",
+    link: "https://playtomic.io/tenant/9ac31a4a-d51c-4e91-96d3-6b65fd8ab9a5?utm_source=app_ios&utm_campaign=share",
+    zone: "Tlaquepaque",
+    lat: 20.673898243956923,
+    lon: -103.40367219909187,
+  },
+  {
+    name: "Padel Guadalupe",
+    tenant_id: "fd6f57ae-caa3-4025-8aa1-bb3cfcf476eb",
+    link: "https://playtomic.io/tenant/fd6f57ae-caa3-4025-8aa1-bb3cfcf476eb?utm_source=app_ios&utm_campaign=share",
+    zone: "Guadalajara",
+    lat: 20.65909078708705,
+    lon: -103.43266338172637,
+  },
+  {
+    name: "Faro Padel Club",
+    tenant_id: "8b38f679-b397-44c9-8b84-058f4663937d",
+    link: "https://playtomic.io/tenant/8b38f679-b397-44c9-8b84-058f4663937d?utm_source=app_ios&utm_campaign=share",
+    zone: "Zapopan",
+    lat: 20.64354097626638,
+    lon: -103.39044464232299,
+  },
+  {
+    name: "Padel isla raza",
+    tenant_id: "cf511f35-bc59-44a7-9e37-5ff54ca4761d",
+    link: "https://playtomic.io/tenant/cf511f35-bc59-44a7-9e37-5ff54ca4761d?utm_source=app_ios&utm_campaign=share",
+    zone: "Guadalajara",
+    lat: 20.633831337895018,
+    lon: -103.38848901843501,
+  },
+  {
+    name: "Padel factory",
+    tenant_id: "2e66f694-4d7e-42ef-967f-d8e94b90db1d",
+    link: "https://playtomic.io/tenant/2e66f694-4d7e-42ef-967f-d8e94b90db1d?utm_source=app_ios&utm_campaign=share",
+    zone: "Zapopan",
+    lat: 20.693354837519717,
+    lon: -103.45211120963569,
+  },
+  {
+    name: "Providencia Padel Club",
+    tenant_id: "517a8e10-1646-430d-bdcc-5cf34ee32f4b",
+    link: "https://playtomic.io/tenant/517a8e10-1646-430d-bdcc-5cf34ee32f4b?utm_source=app_ios&utm_campaign=share",
+    zone: "Guadalajara",
+    lat: 20.68659486484572,
+    lon: -103.39369878446952,
+  },
+  {
+    name: "Draft Padel Americas",
+    tenant_id: "64d2fd83-e7ee-4407-a08e-adecf4e49332",
+    link: "https://playtomic.io/tenant/64d2fd83-e7ee-4407-a08e-adecf4e49332?utm_source=app_ios&utm_campaign=share",
+    zone: "Zapopan",
+    lat: 20.68759803643518,
+    lon: -103.37462480483806,
+  },
+  {
+    name: "Padel Family Gdl",
+    tenant_id: "010d3779-67e5-4d19-8c59-2c7f029e56a7",
+    link: "https://playtomic.io/tenant/010d3779-67e5-4d19-8c59-2c7f029e56a7?utm_source=app_ios&utm_campaign=share",
+    zone: "Tlaquepaque",
+    lat: 20.68827012888777,
+    lon: -103.42775028797583,
+  },
+  {
+    name: "Slice Padel Club",
+    tenant_id: "2d8dff4c-1310-454e-bab5-bd6ee1606655",
+    link: "https://playtomic.io/tenant/2d8dff4c-1310-454e-bab5-bd6ee1606655?utm_source=app_ios&utm_campaign=share",
+    zone: "Guadalajara",
+    lat: 20.68463509573624,
+    lon: -103.44440355810076,
+  },
 
+  //Agregados
+  {
+    name: "RoofTop Padel Club",
+    tenant_id: "2014b839-27e5-4083-8450-7e3f8623a86e",
+    link: "https://playtomic.io/tenant/2014b839-27e5-4083-8450-7e3f8623a86e?utm_source=app_ios&utm_campaign=share",
+    zone: "Zapopan",
+    lat: 20.7070922,
+    lon: -103.3248088,
+  },
+  {
+    name: "Padel Time Club",
+    tenant_id: "96ac0bea-9056-4c8b-9ab9-aec32a019b9f",
+    link: "https://playtomic.io/tenant/96ac0bea-9056-4c8b-9ab9-aec32a019b9f?utm_source=app_ios&utm_campaign=share",
+    zone: "Tlajomulco",
+    lat: 20.54171253,
+    lon: -103.44582443,
+  },
+  {
+    name: "Bounce Padel",
+    tenant_id: "22ad955a-6248-4f43-aec0-fd46a63e969a",
+    link: "https://playtomic.io/tenant/22ad955a-6248-4f43-aec0-fd46a63e969a?utm_source=app_ios&utm_campaign=share",
+    zone: "Guadalajara",
+    lat: 20.7087791,
+    lon: -103.3883157,
+  },
+  {
+    name: "Real Sur Deportivo",
+    tenant_id: "2330bdcd-b93c-48e4-ac53-635b37d7b4f2",
+    link: "https://playtomic.io/tenant/2330bdcd-b93c-48e4-ac53-635b37d7b4f2?utm_campaign=share&utm_source=app_ios",
+    zone: "Tlaquepaque",
+    lat: 20.58132299,
+    lon: -103.42712327,
+  },
+  {
+    name: "Altium Sports Complex",
+    tenant_id: "001aceca-bc01-43ce-907b-fb51ab9e233f",
+    link: "https://playtomic.io/tenant/001aceca-bc01-43ce-907b-fb51ab9e233f?utm_campaign=share&utm_source=app_ios",
+    zone: "Zapopan",
+    lat: 20.7176334,
+    lon: -103.4670446,
+  }
 ];
